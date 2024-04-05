9b454d49089caa9210e5c42913aa38a64f9cac75
mrredo <74524695+mrredo@users.noreply.github.com>
2024-04-05 11:41:25 +0300

    e

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(children: [
         Center(
-          child: Row(
+          child: Container(
+            color: const Color.fromRGBO(126, 120, 210, 1),
+            child: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Kalkulators', style: TextStyle(fontSize: 50),),
+              Text('Kalkulators', style: TextStyle(fontSize: 50, color: Color.fromRGBO(255, 255, 255, 1)),),
             ],
           ),
-        ), //Center
+          ),
+        ), 
+        Image.asset('assets/main_page_image.svg'),
       ],),
     );
   }
