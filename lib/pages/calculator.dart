e02b5d2f7c4b27e97e36814f773a962817b59f76
mrredo <mrredogt@gmail.com>
2024-04-15 16:01:09 +0300

    Update calculator.dart

--- lib/pages/calculator.dart
+++ lib/pages/calculator.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/themes/textColor.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CalculatorMain extends StatelessWidget {
   const CalculatorMain({super.key});
@@ -9,9 +11,9 @@ class CalculatorMain extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Izvēlies kalkulatoru...'),
       ),
-      body: const  Column(
+      body: Column(
         children: [
-          CalculatorMainFul(),
+          CalculatorMainFul(name: "BMI kalkulators", path: "/kalkulators/bmi", imageURL: "/assets/google.svg",),
         ],
       ),
     );
@@ -19,16 +21,46 @@ class CalculatorMain extends StatelessWidget {
 }
 
 class CalculatorMainFul extends StatefulWidget {
-  const CalculatorMainFul({super.key});
 
+  CalculatorMainFul({super.key, required this.name, required this.path, required this.imageURL});
+  String imageURL;
+  String path;
+
+  String name;
   @override
-  State<CalculatorMainFul> createState() => CalculatorMainState();
+  State<CalculatorMainFul> createState() => CalculatorMainState(name: name,path: path, imageURL: imageURL);
 }
 
 class CalculatorMainState extends State<CalculatorMainFul> {
-  CalculatorMainState();
+  CalculatorMainState({required this.name, required this.path,required this.imageURL});
+  String imageURL;
+  String name;
+  String path;
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    return Container(
+      padding: const EdgeInsets.all(5),
+      child: ElevatedButton(
+
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(const Color.fromRGBO(126, 120, 210, 1)),
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, path);
+        },
+        child: Row(children: [
+          Container(
+            padding: const EdgeInsets.only(left: 3, right: 3),
+            child: SvgPicture.asset('assets/main_page_image.svg', width: 50, height: 50,),
+          ),
+          Text(name,
+              textScaler: const TextScaler.linear(2),
+              style: defaultElevatedButtonTextStyle()),
+              
+        ])
+        ),
+    );
+
   }
 }
